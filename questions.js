57e9ffd4a537d5a7c4bf8a57ba6954b8f2de9fad
Alisha <panday.alisha90@gmail.com>
2020-12-01 15:50:53 +0800

    Create an object questions with key and value of title, choices and answes.

---
+++ questions.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+        title:"The function and  var are known as?",
+        choices: [ "keywords", "Data Types", "Declaration statements", "Prototype"],
+        answer: "Declaration statements"
+    },
+
+    {
+        title:"Which one of the following is the correct way for calling the JavaScript code?",
+        choices: [ "Preprocessor", "Trigering event","RMI","Functions/Method" ],
+        answer:"Functions/Method"
+    },
+
+    {
+        title:"Which of the following givenfunctions of the Number Object formats a number with a different number of digits to the right of the decimal?",
+        choices: ["toExponential","toFixed()","toPrecision","toLocaleString"],
+        answer: "toFixed()"
+    },    
+    
+    {
+        title:"Which of the following number object function returns the value of the number?",
+        choices: ["toString()","valueOf()", "toLocaleString", "toPrecision"],
+        answer: "valueOf()"
+    },
+    {
+        title:" In JavaScript the x===y statement implies that:",
+        choices: [ "Both x and y are equal in value, type and reference address as well.", "Both are x and y are equal in value only.","Both are equal in the value and data type","Both are not same at all."],
+        answer:"Both are equal in the value and data type"
+    },
+    {
+        title:" Commonly used data types DO NOT include?",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+];
